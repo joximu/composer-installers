d829559a0f3fd13f2d9b6f5b2a4b016fa1438b53
Joximu <joximu@web.de>
2026-06-17 10:14:48 +0200

    another change "Class iMSCP\Composer\Plugin contains 2 abstract methods..."

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,4 +13,19 @@ class Plugin implements PluginInterface
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
 }
